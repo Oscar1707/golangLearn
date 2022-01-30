04bd297c214df128f15a4cf579912ef65a80a89f
Oscar1707 <39312835+Oscar1707@users.noreply.github.com>
2022-01-29 23:33:24 -0600

    blabla

--- hello.go
+++ hello.go
@@ -8,12 +8,18 @@ func main()  {
 	n1 :=0
 	n2 :=1
 	r  :=0
+	if n2 == 0 {
+		fmt.Println("Hello")
+	
 	 for i := 0; i < 10; i++ {
 		 r = n1+n2
 		 fmt.Println("n1:_ ",n1," n2:_ ",n2, "Res:_ ",r)
 		 n1 = r+n2
 		 r = n1 + n2
 	 }
+	}
+
+	fmt.Println("Me duele la espalda de cargar el peso")
 
 }
 
