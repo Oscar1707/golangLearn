7fd0d5c792d9b8a2027170a7f0feec04e1d79989
Oscar1707 <39312835+Oscar1707@users.noreply.github.com>
2022-01-29 20:51:14 -0600

    add fibonacci

--- hello.go
+++ hello.go
@@ -4,9 +4,15 @@ import "fmt"
 
 func main()  {
 	
-
+	fmt.Println("-----------------[ FIBONACCI ]----------------")
+	n1 :=0
+	n2 :=1
+	r  :=0
 	 for i := 0; i < 10; i++ {
-		 fmt.Println(i)
+		 r = n1+n2
+		 fmt.Println("n1:_ ",n1," n2:_ ",n2, "Res:_ ",r)
+		 n1 = r+n2
+		 r = n1 + n2
 	 }
 
 }
