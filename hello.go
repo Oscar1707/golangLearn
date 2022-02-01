8b99cc45a31feacca323e32a01b1d63d30e0795f
Oscar1707 <39312835+Oscar1707@users.noreply.github.com>
2022-02-01 13:57:30 -0600

    Hello

--- hello.go
+++ hello.go
@@ -5,7 +5,20 @@ import "fmt"
 func main()  {
 	
 	//Add fibonacci serie
-	fibonacci()
+	//fibonacci()
+
+	//Hacker Rank
+	weirdN(3)
+
+}
+
+func weirdN(num float32)  {
+	fmt.Println("-----------------[ WEIRDN ]----------------")
+	fmt.Println("num ",num)
+	
+	var isPair float32 = num / 2
+	fmt.Println("Vamos para sigapr:_ ", isPair)
+	
 
 }
 
